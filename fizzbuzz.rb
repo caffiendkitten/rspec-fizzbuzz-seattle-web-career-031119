5e76abc1d0951a126e0b748ba0dcf1fd4d460a8f
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-02 19:21:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,10 @@
 def fizzbuzz(number)
   if(number % 5 == 0 && number % 3 == 0)
     return "FizzBuzz"
+  elsif (number % 5 == 0)
+    return "Buzz"
+  elsif (number % 3 == 0)
+    return "Fizz"
   else
     return nil
   end
